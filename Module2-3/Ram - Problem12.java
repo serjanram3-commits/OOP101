206fd66d06ac5491893e4bd39249e182f42ceccd
Ser Jan Perzeus Ram <shram@fit.edu.ph>
2026-09-08 13:13:03 +0800

    Fix subtotal calculation and add author header
    Updated the calculation of subtotal to multiply product price by quantity and added a header line with author's name.

--- Module2-3/Ram - Problem12.java
+++ Module2-3/Ram - Problem12.java
@@ -11,19 +11,20 @@ public class M3E2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.println("By: Ser Jan Perzeus H. Ram");
         System.out.print("Enter Product Price: ");
         double product_price = scanner.nextDouble();
         
         System.out.print("Enter quantity: ");
         int quantity = scanner.nextInt();
         
-        double subtotal = product_price + quantity;
+        double subtotal = product_price * quantity;
         double VAT = subtotal * 0.12;
         double total = VAT + subtotal;
         
-        System.out.println("Subtotal: " + subtotal);
-        System.out.println(" VAT: " + VAT);
-        System.out.println("Total: " + total);
+        System.out.println("Subtotal:PHP " + subtotal);
+        System.out.println(" VAT:PHP " + VAT);
+        System.out.println("Total:PHP " + total);
         
         scanner.close();
     }

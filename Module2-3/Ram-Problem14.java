8cd451f88c4eb171e4b230b247d06a4f27a77f65
Ser Jan Perzeus Ram <shram@fit.edu.ph>
2026-09-08 13:14:14 +0800

    Add Ram-Problem14.java file

--- Module2-3/Ram-Problem14.java
+++ Module2-3/Ram-Problem14.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class M3E4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("By: Ser Jan Perzeus H. Ram");
         
         System.out.println("Enter weight (in kilograms): ");
         double weight = scanner.nextDouble();
@@ -19,7 +20,7 @@ public class M3E4 {
         
         double BMI = weight / (height * height);
         
-        System.out.println("BMI: approximately" + BMI);
+        System.out.println("BMI: approximately " + BMI);
         scanner.close();
     }
 }
